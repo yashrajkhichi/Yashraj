fc6a6aa199d9684f591134e14feb853b75ad5243
yashrajkhichi <142218330+yashrajkhichi@users.noreply.github.com>
2023-08-21 13:49:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer"],
+        strings: ["Full-Stack Developer","Front-end Developer","Back-End Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
